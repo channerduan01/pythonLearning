ddd277bb44fff464b688f516e9dbde15bcd947bb
channerduan <channerduan@gmail.com>
2016-01-28 16:55:07 +0000

    performance check

--- DigitRecognizeKaggle_rForest.py
+++ DigitRecognizeKaggle_rForest.py
@@ -18,13 +18,15 @@ with Timer() as t:
     train = [x[1:] for x in dataset]
     test = genfromtxt(open('test.csv','r'), delimiter=',', dtype='f8')[1:]
     rf = RandomForestClassifier(n_estimators=100)
-    print "=> load data spent: %s s" % t.secs
+print "=> load data spent: %s s" % t.secs
+
 with Timer() as t:
     print 'start train...'
     rf.fit(train, target)
-    print "=> train spent: %s s" % t.secs
+print "=> train spent: %s s" % t.secs
+
 with Timer() as t:
     print 'output result...'
     res = rf.predict(test)
     savetxt('submission.csv', np.c_[np.arange(1, res.size+1), res], delimiter=',', header='ImageId,Label', fmt='%d', comments='')
-    print "=> output spent: %s s" % t.secs
+print "=> output spent: %s s" % t.secs
