45be36c96f80ee2eb2bd4456eef4a0320c828125
channerduan <channerduan@gmail.com>
2016-02-03 18:40:38 +0000

    detection

--- edgeDetect.py
+++ edgeDetect.py
@@ -10,6 +10,9 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 from timer import Timer
+from mpl_toolkits.mplot3d.axes3d import Axes3D
+from matplotlib import cm  
+from matplotlib.ticker import LinearLocator, FormatStrFormatter  
 
 
 def readImage(path):
@@ -73,7 +76,6 @@ def zeroCross(src):
                 image[i][j] = 255
     return image
 
-
 def drawFigures(params):
     length = len(params)
     if (length < 2 or length%2 == 1):
@@ -93,19 +95,26 @@ def drawFigures(params):
     
 def compareDifferentTypes(image):
     with Timer() as t:
-        test1 = imageFilter(image,createGaussianTemplate(11,2))
-        test1 = imageFilter(test1,createLaplacianTemplate(11))
-        test1 = zeroCross(test1)
+        test1 = imageFilter(image,createGaussianTemplate(9,2))
+        test1 = imageFilter(test1,createLaplacianTemplate(9))
     print "=> Gaussian+Laplacian spent: %s s" % t.secs
+    test1 = zeroCross(test1)
     with Timer() as t:
-        test2 = imageFilter(image,createLoGTemplate(11,2))
-        test2 = zeroCross(test2)
-    print "=> loG spent: %s s" % t.secs
+        test2 = imageFilter(image,createLoGTemplate(9,2))
+    print "=> LoG spent: %s s" % t.secs    
+    test2 = zeroCross(test2)
     with Timer() as t:
-        test3 = marrByCv(image,3,1.0)
-    print "=> opencv marr spent: %s s" % t.secs
-    drawList = ['original',image,'opencv marr-hildreth',test3,'gaussian+laplacian',test1,'loG',test2]
-    drawFigures(drawList)     
+        test3 = marrByCv(image,9,2)
+        test3 = normalize(test3)
+    print "=> opencv Gaussian+Laplacian spent: %s s" % t.secs
+    test3 = zeroCross(test3)
+    with Timer() as t:
+        test4 = imageFourierFilter(image,createGaussianTemplate(9,2))
+        test4 = normalize(test4)
+    print "=> Fourier LoG spent: %s s" % t.secs
+    test4 = zeroCross(test4)
+    drawList = ['gaussian+laplacian',test1,'LoG',test2,'opencv gaussian+laplacian',test3,'Fourier LoG',test4]
+    drawFigures(drawList)
     return
     
 def compareParametersOfloG(image):
@@ -125,18 +134,167 @@ def compareParametersOfloG(image):
         drawList.append(imgList[i])
     drawFigures(drawList)
     return
-
-def main(path):
-    plt.gray()
-    image = readImage(path)
     
-#    compareDifferentTypes(image)
-    compareParametersOfloG(image)
+def extractTheCentreOfMatrix(matrix,size=1):
+    size = np.floor(size/2);
+    s = matrix.shape;
+    cx = np.floor(s[0]/2)
+    cy = np.floor(s[1]/2)
+    return matrix[cx-size:cx+size+1,cy-size:cy+size+1]
+    
+def deleteTheCentreOfMatrix(matrix,size=1):
+    size = np.floor(size/2);
+    s = matrix.shape;
+    cx = np.floor(s[0]/2)
+    cy = np.floor(s[1]/2)
+    matrix[cx-size:cx+size+1,cy-size:cy+size+1] = 0
+    return matrix
+
+# tricky function... should add more condition check in future...
+def expandMatrix(matrix,size):
+    result = np.zeros((size[0],size[1]),dtype=matrix.dtype)
+    cx = np.float(matrix.shape[0]/2)
+    cy = np.float(matrix.shape[1]/2)
+    ccx = np.float(result.shape[0]/2)
+    ccy = np.float(result.shape[1]/2)    
+    result[ccx-cx:ccx+cx+1,ccy-cy:ccy+cy+1] = matrix
+    return result
+
+def createFourierAnalysisForImage(image,size,avoidCentre=True):
+    fourier = np.abs(np.fft.fftshift(np.fft.fft2(image)))
+    # the centre is often too large, so remove it for better visualization
+    if avoidCentre:
+        fourier[np.floor(fourier.shape[0]/2), np.floor(fourier.shape[1]/2)] = 0
+    min_ = np.min(fourier)
+    max_ = np.max(fourier)
+    # map to [0,1]
+    fourier = (fourier-min_)/(max_-min_)
+    # extract center pixels for better visualization
+    fourier = extractTheCentreOfMatrix(fourier,size)
+    fourier = np.round(fourier * 255)
+    # replenish the centre
+    if avoidCentre:
+        fourier[np.floor(fourier.shape[0]/2), np.floor(fourier.shape[1]/2)] = 255
+    return fourier.astype(np.uint8)
+
+def lowpassFourierFilter(image,width):
+    res = np.fft.fftshift(np.fft.fft2(image))
+    res = extractTheCentreOfMatrix(res,width)
+    res = expandMatrix(res,image.shape)
+    res = np.fft.ifft2(np.fft.ifftshift(res))
+    return np.abs(res)
+
+def highpassFourierFilter(image,width):
+    res = np.fft.fftshift(np.fft.fft2(image))
+    res = deleteTheCentreOfMatrix(res,width)
+    res = np.fft.ifft2(np.fft.ifftshift(res))
+    return np.abs(res)   
+
+def draw3D(Z):
+    X = range(Z.shape[0])    
+    Y = range(Z.shape[1])   
+    X, Y = np.meshgrid(X,Y)  
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1,projection='3d')
+    ax.axis('off')
+    ax.plot_surface(X,Y,Z,rstride=1,cstride=1,cmap=cm.coolwarm,linewidth=0,antialiased=False)     
     return
 
+def show3DforloG(size,sigma):
+    Z = createLoGTemplate(size,sigma)
+    X = range(size)
+    Y = range(size)
+    X, Y = np.meshgrid(X,Y)  
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 2, 1, projection='3d')
+    ax.axis('off')
+    ax.plot_surface(X,Y,Z,rstride=1,cstride=1,cmap=cm.coolwarm,linewidth=0,antialiased=False)   
+    return
+    
+def mapTo255(matrix):
+    max_ = np.max(matrix)
+    min_ = np.min(matrix)
+    matrix = (matrix-min_)/(max_-min_)
+    matrix *= 255
+    matrix = np.round(matrix)
+    return matrix.astype(np.uint8)
+
+def normalize(matrix):
+    mean_ = np.mean(matrix)
+    std_ = np.std(matrix)
+    max_ = np.max(matrix)
+    min_ = np.min(matrix)
+#    return (matrix-mean_)/std_
+    return (matrix-mean_)/(max_-min_)
+
+def demoLowHighpassFilter(image):
+    plt.figure()
+    plt.axis('off')
+    plt.imshow(lowpassFourierFilter(image,21))
+    plt.figure()
+    plt.axis('off')
+    plt.imshow(highpassFourierFilter(image,21))
+    return
+    
+def imageFourierFilter(src,template):
+    return np.abs(np.fft.fftshift(np.fft.ifft2(np.fft.fft2(image)*np.fft.fft2(expandMatrix(template,image.shape)))))
+
+def demoProcessOfFourier(image,template):
+    list_ = []
+    list_.append('original')
+    list_.append(image)
+    p1 = np.fft.fftshift(np.fft.fft2(image))
+    list_.append('original fourior')
+    list_.append(np.abs(p1))
+    list_.append('template')
+    list_.append(template)
+    p2 = np.fft.fftshift(np.fft.fft2(expandMatrix(template,image.shape)))
+    list_.append('template fourior')
+    list_.append(np.abs(p2))
+    p3 = p1*p2
+    list_.append('multiply fourior')
+    list_.append(np.abs(p3))
+    p4 = np.abs(np.fft.fftshift(np.fft.ifft2(p3)))
+    list_.append('inverse fourior')
+    list_.append(p4)
+    p5 = normalize(p4)
+    list_.append('normalize')
+    list_.append(p5)
+    p6 = zeroCross(p5)
+    list_.append('zero cross')
+    list_.append(p6)
+    drawFigures(list_)
+    return
+
+def demoImageFourierForm(image,zoomSize,avoidCentre=True):
+    plt.figure()
+    plt.axis('off')
+    plt.imshow(createFourierAnalysisForImage(image,21,avoidCentre))
+    return
+#def fourier
 
 inputfile='nevermore.png'
 #inputfile='nature.png'
 #inputfile='lenna.png'
-main(inputfile)
 
+plt.gray()
+image = readImage(inputfile)
+
+
+#
+#plt.figure()
+#plt.axis('off')
+#test = expandMatrix(createLoGTemplate(21,3),image.shape)
+#plt.imshow(createFourierAnalysisForImage(test,image.shape[0],False))
+
+#plt.figure()
+#plt.axis('off')
+#plt.imshow(imageFilter(image,createLoGTemplate(21,3)))
+
+compareDifferentTypes(image)
+#compareParametersOfloG(image)
+#fig = show3DforloG(21,2.6)
+
+#demoImageFourierForm(image,21,True)
+#demoLowHighpassFilter(image)
+#demoProcessOfFourier(image,createLoGTemplate(21,3))
